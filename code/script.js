990281c49e3457dc705ccbb9eb72ad0361b01e3f
tpku <tpku@icloud.com>
2022-11-18 16:03:55 +0100

    Update script. Structure change and comments

--- code/script.js
+++ code/script.js
@@ -1,19 +1,19 @@
+// Navbar Scroll Event
+const navBar = document.querySelector(".nav-bar");
+// Navbar Desktop
+window.addEventListener('scroll', function (e) {
+  if (window.scrollY > 971) {
+    navBar.classList.add("active-desktop")
+  } else {
+    navBar.classList.remove("active-desktop")
+  }
+});
+
+// Products Card Hover Effect
 const cardsHover = document.querySelectorAll(".product.card");
 const textContainer = document.querySelector(".text-container");
 const btnContainer = document.querySelector(".btn-container-index")
 
-
-
-// cardsHover.forEach((card) => {
-//   card.addEventListener("mouseover", () => {
-//     textContainer.classList.toggle('product-hover');
-//     btnContainer.classList.toggle('product-hover');
-//   });
-//   card.addEventListener("mouseleave", () => {
-//     textContainer.classList.toggle('product-hover');
-//     btnContainer.classList.toggle('product-hover');
-//   });
-// });
 cardsHover.forEach((card) => {
   card.addEventListener("mouseenter", (e) => {
     textContainer.classList.toggle('product-hover');
@@ -24,13 +24,3 @@ cardsHover.forEach((card) => {
     btnContainer.classList.toggle('product-hover');
   });
 });
-
-const navBar = document.querySelector(".nav-bar");
-
-window.addEventListener('scroll', function (e) {
-    if (window.scrollY > 971) {
-        navBar.classList.add("active-desktop")
-    } else {
-        navBar.classList.remove("active-desktop")
-    }
-  });
